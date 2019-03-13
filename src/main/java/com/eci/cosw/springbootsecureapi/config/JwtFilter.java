0ace0a5f331d19e212f3834677d22e67b4d9b4a0
sancarbar <sancarbar@gmail.com>
2019-03-12 22:41:51 -0500

    Updated gradle dependencies.

--- src/main/java/com/eci/cosw/springbootsecureapi/config/JwtFilter.java
+++ src/main/java/com/eci/cosw/springbootsecureapi/config/JwtFilter.java
@@ -1,5 +1,8 @@
 package com.eci.cosw.springbootsecureapi.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureException;
 import org.springframework.web.filter.GenericFilterBean;
 
 import javax.servlet.FilterChain;
@@ -43,15 +46,15 @@ public class JwtFilter
 
             final String token = authHeader.substring( 7 );
 
-//            try
-//            {
-//                final Claims claims = Jwts.parser().setSigningKey( "secretkey" ).parseClaimsJws( token ).getBody();
-//                request.setAttribute( "claims", claims );
-//            }
-//            catch ( final SignatureException e )
-//            {
-//                throw new ServletException( "Invalid token" );
-//            }
+            try
+            {
+                final Claims claims = Jwts.parser().setSigningKey( "secretkey" ).parseClaimsJws( token ).getBody();
+                request.setAttribute( "claims", claims );
+            }
+            catch ( final SignatureException e )
+            {
+                throw new ServletException( "Invalid token" );
+            }
 
             filterChain.doFilter( servletRequest, response );
         }
--- src/main/java/com/eci/cosw/springbootsecureapi/controller/UserController.java
+++ src/main/java/com/eci/cosw/springbootsecureapi/controller/UserController.java
@@ -2,6 +2,8 @@ package com.eci.cosw.springbootsecureapi.controller;
 
 import com.eci.cosw.springbootsecureapi.model.User;
 import com.eci.cosw.springbootsecureapi.service.UserService;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.ServletException;
+import java.util.Date;
 
 /**
  * @author Santiago Carrillo
@@ -51,8 +54,8 @@ public class UserController
             throw new ServletException( "Invalid login. Please check your name and password." );
         }
 
-//        jwtToken = Jwts.builder().setSubject( username ).claim( "roles", "user" ).setIssuedAt( new Date() ).signWith(
-//            SignatureAlgorithm.HS256, "secretkey" ).compact();
+        jwtToken = Jwts.builder().setSubject( username ).claim( "roles", "user" ).setIssuedAt( new Date() ).signWith(
+            SignatureAlgorithm.HS256, "secretkey" ).compact();
 
         return new Token( jwtToken );
     }
@@ -60,23 +63,23 @@ public class UserController
     public class Token
     {
 
-        String access_token;
+        String accessToken;
 
 
-        public Token( String access_token )
+        public Token( String accessToken )
         {
-            this.access_token = access_token;
+            this.accessToken = accessToken;
         }
 
 
         public String getAccessToken()
         {
-            return access_token;
+            return accessToken;
         }
 
         public void setAccessToken( String access_token )
         {
-            this.access_token = access_token;
+            this.accessToken = access_token;
         }
     }
 
